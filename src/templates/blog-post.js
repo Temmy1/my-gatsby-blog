99902beccb89b2685b6e844e0afb3e99e61c870d
Artom Ryzhkov <temmyriz@gmail.com>
2019-06-19 14:20:13 +0300

    ...

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,7 +2,7 @@ import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import Container from "../components/container"
-import SEO from "../components/SEO"
+import SEO from "../components/seo"
 
 export default ({ data }) => {
   const post = data.markdownRemark
